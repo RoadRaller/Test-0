88897a2a1331e7d8e9e45593f921b0bb3dc63eba
RoadRaller <azzzx.aps@gmail.com>
2026-03-03 10:47:00 +0000

    git commit

--- test0.py
+++ test0.py
@@ -1,1 +1,21 @@
-print('hello')
+def printlst(lst):
+  for item in lst:
+    print(item)
+
+menu = ['airport','flight','price','clear','quit']
+
+info = []
+txt = open("airport.txt","r")
+info = readlines(txt)
+for i in range(len(info)):
+  info[0] = info[0].split(',')
+print(info)
+inp = None
+while inp != 'quit':
+  printlst(menu)
+
+  inp = input()
+
+  
+  
+print('end')
