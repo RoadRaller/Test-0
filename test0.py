090e0eaf016b1c6daaeb3aaa13b0149751d8c537
RoadRaller <azzzx.aps@gmail.com>
2026-03-05 14:29:28 +0000

    git commit

--- test0.py
+++ test0.py
@@ -2,20 +2,45 @@ def printlst(lst):
   for item in lst:
     print(item)
 
-menu = ['airport','flight','price','clear','quit']
+def airport(info):
+  bport = input('Enter British Airport')
+  if bport != 'LPL' and bport != 'BOH':
+    print('wrong airport')
+    return
+  
+  iport = input('Enter International Airport')
+  for port in info:
+    if iport in port:
+      print(port)
+      return
+  print('not found')
+  return
 
+  
+
+
+#get file
 info = []
-txt = open("airport.txt","r")
-info = readlines(txt)
+file = open("airplane.txt","r")
+info = file.readlines()
 for i in range(len(info)):
-  info[0] = info[0].split(',')
-print(info)
+  info[i] = info[i].split(',')
+  info[i][-1] = info[i][-1][0:-1]
+del info[-1]
+printlst(info)
+
+
+
+
+menu = ['airport','flight','price','clear','quit']
 inp = None
 while inp != 'quit':
   printlst(menu)
-
+  
   inp = input()
 
+  if inp == 'airport':
+    airport(info)
   
-  
+file.close()
 print('end')
